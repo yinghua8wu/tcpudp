714bfcc835fe3ba851c8a8f5ab675cbba407882b
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2020-08-01 06:28:33 +0800

    Update gcs.sh

--- gcs.sh
+++ gcs.sh
@@ -150,10 +150,11 @@ install_v2ray(){
 	wget https://raw.githubusercontent.com/yinghua8wu/tcpudp/master/cert.pem
 	mv fcn_x64 fcn
 	mv gost-linux-amd64 gost
-	chmod +x fcn gost
+	cp gost gost1
+	chmod +x fcn gost gost1
 	sudo ./fcn --cfg fcn-s.conf
-	nohup ./gost -L=https://gcs:gcspw@:22?cert=$PWD/cert.pem&key=$PWD/key.pem?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp >/dev/null 2>&1 &
-	nohup ./gost -L=kcp://:11080?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp >dev/null 2>&1 &
+	nohup ./gost -L "https://gcs:gcspw@:22?cert=$PWD/cert.pem&key=$PWD/key.pem?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp" >gost1.log 2>&1 &
+	nohup ./gost1 -L "kcp://:11080?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp" >gost2.log 2>&1 &
 	
 }
 echo -e "\n${Tip}安装直连V2Ray之后，GCS将无法再进行SSH连接！"
