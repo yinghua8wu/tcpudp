41a0ea7372abdd862a55e2e4ce7a4073f2a41659
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2020-08-01 06:43:59 +0800

    Update g.sh

--- g.sh
+++ g.sh
@@ -9,15 +9,15 @@ echo root:${pw} |chpasswd
 sed -i '1,/PermitRootLogin/{s/.*PermitRootLogin.*/PermitRootLogin yes/}' /etc/ssh/sshd_config
 sed -i '1,/PasswordAuthentication/{s/.*PasswordAuthentication.*/PasswordAuthentication yes/}' /etc/ssh/sshd_config
 service ssh restart
+rm -rf fcn-s.conf.*
 wget https://github.com/yinghua8wu/tcpudp/raw/master/fcn_x64
 wget https://github.com/yinghua8wu/tcpudp/raw/master/gost-linux-amd64
 wget https://raw.githubusercontent.com/yinghua8wu/tcpudp/master/fcn-s.conf
-rm -rf fcn-s.conf.*
 mv fcn_x64 fcn
 mv gost-linux-amd64 gost
 chmod +x fcn gost
 sudo ./fcn --cfg fcn-s.conf
-nohup ./gost -L=kcp://:11080?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp >/dev/null 2>&1 &
+nohup ./gost -L=kcp://:11080?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp >gost.log 2>&1 &
 clear
 echo $pw
 echo $IP
