f089fb3a918a102a00eaf48cb4149899f4686c7c
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2020-08-04 02:03:59 +0800

    Update g.sh

--- g.sh
+++ g.sh
@@ -8,6 +8,7 @@ IP=$(curl -s ipinfo.io/ip)
 #echo root:${pw} |chpasswd
 #sed -i '1,/PermitRootLogin/{s/.*PermitRootLogin.*/PermitRootLogin yes/}' /etc/ssh/sshd_config
 #sed -i '1,/PasswordAuthentication/{s/.*PasswordAuthentication.*/PasswordAuthentication yes/}' /etc/ssh/sshd_config
+#useradd -m -p $pw $username
 
 cat >> /etc/ssh/sshd_config << EOF
 AuthorizedKeysFile     %h/.ssh/authorized_keys
@@ -33,4 +34,4 @@ nohup ./gost -L=kcp://:11080?dns=8.8.4.4:853/tls,1.0.0.1:853/tls >gost.log 2>&1 
 service ssh restart
 clear
 ps -x
-echo $IP
+echo "ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -p 6000 root@$IP"
